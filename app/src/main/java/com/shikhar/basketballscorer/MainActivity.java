793dcb3a98f46a44dedb687f0cdf1cd78b320e29
shikhar30 <shikhar.bhargava30@gmail.com>
2016-06-18 20:43:45 +0530

    Addition of timer

--- app/src/main/java/com/shikhar/basketballscorer/MainActivity.java
+++ app/src/main/java/com/shikhar/basketballscorer/MainActivity.java
@@ -2,30 +2,29 @@ package com.shikhar.basketballscorer;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-
-
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    int score;
-    int scoreB;
+    int score;//scorer for team A
+    int scoreB;//scorer for team B
     int n=0;
     int m=0;//keeping the count of the array list
     public static final String PREFS_NAME = "MyPrefsFile";
     int tA,tB,DA,DB,FA,Fb;
      String team1,team2;
 
-
+    private int seconds; // to calculate the seconds
+    private boolean running;// to save the state when the button is pressed in the boolean form
 
     public ArrayList<String> namesA= new ArrayList<>();//List to save the names of the team A
     public ArrayList<String> namesB=new ArrayList<>();//list to save the names of the team B
@@ -39,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        runTimer();
         if (savedInstanceState != null)
         {
             // Restore value of members from saved state
@@ -48,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             ScoreTeamA=savedInstanceState.getIntegerArrayList("scoreA");
             ScoreTeamB=savedInstanceState.getIntegerArrayList("scoreB");
         }
-        else ;
+        runTimer();
 
     }
     /*triple pointer for team A*/
@@ -116,14 +115,16 @@ public class MainActivity extends AppCompatActivity {
         displayScoreB(scoreB);
         tA=0;tB=0;DA=0;DB=0;FA=0;Fb=0;
     }
-
+//Submit button click. and sending of data to the new intent
     public void Submit(View v)
     {
         n+=1;
         m+=1;
         addToStringList();
         addToIntegerList();
-      Intent i=new Intent(this,Submit.class);
+        running=false;
+
+      Intent i=new Intent(this,Submit.class);seconds=0;
         i.putExtra("ThreePointerA",tA);
         i.putExtra("ThreepointerB",tB);
         i.putExtra("TwopointerA",DA);
@@ -141,7 +142,7 @@ public class MainActivity extends AppCompatActivity {
         displayScoreB(scoreB);*/
 
     }
-
+// making the array list of the names
     public void addToStringList(){
 
         EditText et=(EditText)findViewById(R.id.teamname);
@@ -157,7 +158,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
+//making the arraylist of the scores
 
 
     public void addToIntegerList(){
@@ -168,7 +169,6 @@ public class MainActivity extends AppCompatActivity {
         ScoreTeamA.add(m-1,scoreA);
         ScoreTeamB.add(m-1,scoreB);
     }
-
 
 
     public void onSaveInstanceState(Bundle savedInstanceState)
@@ -183,6 +183,47 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(savedInstanceState);
     }
 
+//timer start button
+    public void timerstarter(View view)
+    {
+        running=true;
+    }
+
+    //timer stop button
+
+    public void timerStopper(View view)
+    {
+        running=false;
+    }
+//reset button for the timer
+    public void resettimer(View v)
+    {
+        running=false;
+        seconds=0;
+    }
+//handler and the method for the new thread of
+    //timer. provides running of the timer in the background
+    public void runTimer()
+    {
+        final TextView tv=(TextView)findViewById(R.id.timer);
+        final Handler mHandler= new Handler() ;
+       mHandler.post(new Runnable() {
+           @Override
+           public void run() {
+               int hours=seconds/3600;
+               int minutes=(seconds%3600)/60;
+                int secs=seconds%60;
+               String time= String.format("%02d:%02d:%02d",hours,minutes,secs);
+               tv.setText(time);
+               if(running) {
+                   seconds++;
+               }
+               mHandler.postDelayed(this,1000);
+               }
+
+       });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
